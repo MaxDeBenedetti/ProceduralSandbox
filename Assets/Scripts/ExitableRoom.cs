725fc53cfa0188fdffef1f805e72bb8e2c002575
MaxDeBenedetti <modsme@gmail.com>
2014-04-30 09:02:41 -0500

    We are close

---
+++ Assets/Scripts/ExitableRoom.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	public class ExitableRoom : MonoBehaviour {
+
+	public GameObject[] hallways;
+	public GameObject[] walls;
+	public GameObject[] exits;
+	public int roomValue;
+	public int roomLocX, roomLocY;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Connects this room to other rooms using hallways
+	//Be sure to assign roomValue first
+	public void ConnectRoom(){
+		Vector3 exit;
+
+		//north
+		exit = walls[0].transform.position;
+		if (roomValue % 2 == 0) {
+			Instantiate(hallways[0],exit,Quaternion.identity);
+		}
+		else{
+			Instantiate(walls[0],exit,Quaternion.identity);
+		}
+
+		//east
+		exit = exits[1].transform.position;
+		if (roomValue % 3 == 0) {
+			Instantiate(hallways[1],exit,Quaternion.identity);
+		}
+		else{
+			Instantiate(walls[1],exit,Quaternion.identity);
+		}
+
+		//south
+		if(!(roomValue % 5 == 0)){
+			exit = exits[2].transform.position;
+			Instantiate(walls[2],exit,Quaternion.identity);
+		}
+
+		//west
+		if(!(roomValue % 7 == 0)){
+			exit = exits[3].transform.position;
+			Instantiate(walls[3],exit,Quaternion.identity);
+		}
+
+	}
+
+	public int RoomValue {
+		get {
+			return this.roomValue;
+		}
+		set {
+			roomValue = value;
+		}
+	}
+
+	public int RoomLocX {
+		get {
+			return this.roomLocX;
+		}
+		set {
+			roomLocX = value;
+		}
+	}
+
+	public int RoomLocY {
+		get {
+			return this.roomLocY;
+		}
+		set {
+			roomLocY = value;
+		}
+	}
+
+}
+
--- Assets/Scripts/LevelManager2.cs
+++ Assets/Scripts/LevelManager2.cs
@@ -1,93 +1,97 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class LevelManager2 : MonoBehaviour
 {
-
-    public int[,] grid;
-    public int gridWidth, gridHeight;
+	
+	public int[,] grid;
+	public int gridWidth, gridHeight;
 	public TestCube cub;
-    public int numOfRooms;
-
-
-    // Use this for initialization
-    void Start()
-    {
-        grid = new int[gridWidth, gridHeight];
-        for (int i = 0; i < gridWidth; i++)
-        {
-            for (int j = 0; j < gridHeight; j++)
-            {
-                grid[i, j] = 0;
-            }
-        }
-
-        int firstX = gridWidth / 2;
-        int firstY = gridHeight / 2;
-
-        grid[firstX, firstY] = 1;
-
-        RoomAdder adder = new RoomAdder(firstX, firstY, grid, numOfRooms);
-        int[] room;
-        while (adder.RoomCount < numOfRooms)
-        {
-            room = RoomAdder.PickRoom(adder.RoomCount, adder.RoomList);
-            NeighborCheck(grid, room[0], room[1], adder);
-            adder.Process();
-        }
-
-        RoomEvaluator eval = new RoomEvaluator(grid, numOfRooms);
-
-        for (int i = 0; i < gridWidth; i++)
-        {
-            for (int j = 0; j < gridHeight; j++)
-            {
-                if (grid[i, j] != 0)
-                {
-                    eval.AddCenter(i, j);
-                    NeighborCheck(grid, i, j, eval);
-                    eval.Process();
-                }
-            }
-        }
-
-
+	public int numOfRooms;
+	public int roomLength, hallLength;
+	public GameObject player;
+	
+	
+	// Use this for initialization
+	void Start()
+	{
+		grid = new int[gridWidth, gridHeight];
+		for (int i = 0; i < gridWidth; i++)
+		{
+			for (int j = 0; j < gridHeight; j++)
+			{
+				grid[i, j] = 0;
+			}
+		}
+		
+		int firstX = gridWidth / 2;
+		int firstY = gridHeight / 2;
+		
+		grid[firstX, firstY] = 1;
+		
+		RoomAdder adder = new RoomAdder(firstX, firstY, grid, numOfRooms);
+		int[] room;
+		while (adder.RoomCount < numOfRooms)
+		{
+			room = RoomAdder.PickRoom(adder.RoomCount, adder.RoomList);
+			NeighborCheck(grid, room[0], room[1], adder);
+			adder.Process();
+		}
+		
+		RoomEvaluator eval = new RoomEvaluator(grid, numOfRooms);
+		
+		for (int i = 0; i < gridWidth; i++)
+		{
+			for (int j = 0; j < gridHeight; j++)
+			{
+				if (grid[i, j] != 0)
+				{
+					eval.AddCenter(i, j);
+					NeighborCheck(grid, i, j, eval);
+					eval.Process();
+				}
+			}
+		}
+		
+		
 		roomify();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+	
 	/*
 	 * checks the rooms around a specified room and then performs the action defined by process object p
 	 */
-    public void NeighborCheck(int[,] grid, int row, int colum, ProcessObject p)
-    {
-        if (row - 1 >= 0)
-            p.AddNeighbor(row - 1, colum);
-        if (row + 1 < gridWidth)
-            p.AddNeighbor(row + 1, colum);
-        if (colum - 1 >= 0)
-            p.AddNeighbor(row, colum - 1);
-        if (colum + 1 < gridHeight)
-            p.AddNeighbor(row, colum + 1);
-    }
-
+	public void NeighborCheck(int[,] grid, int row, int colum, ProcessObject p)
+	{
+		if (row - 1 >= 0)
+			p.AddNeighbor(row - 1, colum);
+		if (row + 1 < gridWidth)
+			p.AddNeighbor(row + 1, colum);
+		if (colum - 1 >= 0)
+			p.AddNeighbor(row, colum - 1);
+		if (colum + 1 < gridHeight)
+			p.AddNeighbor(row, colum + 1);
+	}
+	
 	/*
 	 * Instantiates the rooms
 	 */
 	private void roomify(){
-		Vector3 startPos = new Vector3(-6f,3f,0f);//Hardcoded for now as it will not matter once every room has a camera
+		Vector3 startPos = new Vector3(-6f,3f,0f);//Hardcoded, for now, as it will not matter once every room has a camera
 		//Instantiate(cub,startPos,Quaternion.identity);
-
+		bool playerNotAdded = true;
 		TestCube tc;
 		for(int i =0; i < gridWidth; i++){
 			for(int j=0; j <gridHeight; j++){
-
+				
 				if(grid[i,j] != 0){
+					if(playerNotAdded)
+						player.rigidbody.position = new Vector3(startPos.x+j,startPos.y-i,-2f);
 					tc = (TestCube)Instantiate(cub,new Vector3(startPos.x+j,startPos.y-i,startPos.z),Quaternion.identity);
 					tc.setNum(grid[i,j]);
 				}
