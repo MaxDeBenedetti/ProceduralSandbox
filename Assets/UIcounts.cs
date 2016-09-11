347eb2ae63e010d5398b3b317235dd46e9002ac4
MaxDeBenedetti <modsme@gmail.com>
2016-09-11 12:17:06 -0500

    ui tracks kils and floor

---
+++ Assets/UIcounts.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIcounts : MonoBehaviour {
+
+    public Text killCount, floorCount;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        killCount.text = "" + PlayerPrefs.GetInt(Names.currentKills);
+        floorCount.text = "" + PlayerPrefs.GetInt(Names.currentFloor);
+	}
+}
